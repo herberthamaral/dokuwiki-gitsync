17be06cd1635cb1af8f19f2a8ddc7805ee8a5798
Herberth Amaral <herberthamaral@gmail.com>
2010-11-22 08:53:08 -0200

    First commit

---
+++ gitsync.php
@@ -0,0 +1,15 @@
+<?php
+
+if(!defined('DOKU_INC')) die();
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once DOKU_PLUGIN.'action.php';
+
+class action_plugin_gitsync_gitsync
+{
+    function register(&$controller)
+    {
+        
+    }
+}
+
+?>
